00316740fc257c6bebe7b87e88a52b13020952db
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-10 18:11:42 +0530

    Create Akhila.java

---
+++ Akhila.java
@@ -0,0 +1,10 @@
+package com.Multi_threading;
+public class Akhila extends Thread {
+	public void run()
+    {
+  	  for(int i=0;i<11;i++)
+  	  {
+  		  System.out.println("i:"+i);
+  	  }
+    }
+}
