de79530a941b0679b5a352cf6ad9bce0dad792ae
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-11 19:04:01 +0530

    Create Thilak.java

---
+++ Thilak.java
@@ -0,0 +1,10 @@
+package com.Multi_threading;
+public class Tilak implements Runnable {
+      public void run()
+      {
+    	  for(int i=0;i<11;i++)
+    	  {
+    		  System.out.println("i:"+i);
+    	  }
+      }
+}
