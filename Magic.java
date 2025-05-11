710a1d43428248bd7912186cd43429e13ed7a6f8
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-11 18:59:53 +0530

    Create Magic.java

---
+++ Magic.java
@@ -0,0 +1,12 @@
+package com.Multi_threading;
+public class Magic {
+	synchronized public void Akhilad(int i)
+	{
+		System.out.println("Akhila:"+i);
+	}
+	synchronized public void Thilaks(int i)
+	{
+		System.out.println("Tilak:"+i);
+		
+	}
+}
